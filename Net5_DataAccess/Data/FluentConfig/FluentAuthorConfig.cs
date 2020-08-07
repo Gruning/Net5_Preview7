6f369b7dc0ab5c0452d95132a81b45e6bd0e779f
AlbertoGruning-Ubuntu <alberto.gruning.zen@gmail.com>
2020-08-08 00:13:32 +0100

    add book config

---
+++ Net5_DataAccess/Data/FluentConfig/FluentAuthorConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Net5_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5_DataAccess.Data.FluentConfig
+{
+    public class FluentAuthorConfig : IEntityTypeConfiguration<FluentAuthor>
+    {
+        public void Configure(EntityTypeBuilder<FluentAuthor> modelBulder)
+        {
+            modelBuilder.HasKey(a => a.Author_Id);
+            modelBuilder.Property(a => a.FirstName).IsRequired();
+            modelBuilder.Property(a => a.LastName).IsRequired();
+            modelBuilder.Ignore(a => a.FullName);
+
+        }
+    }
+}
