216bef2f260376663a1970bcffec9b067ad9ef34
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-09 15:14:42 -0300

    TODO implement FluenBook

--- Net5_DataAccess/Data/ApplicationDbContext.cs
+++ Net5_DataAccess/Data/ApplicationDbContext.cs
@@ -38,27 +38,27 @@ namespace Net5_DataAccess.Data
             modelBuilder.Entity<FluentBookDetail>().HasKey(b => b.BookDetail_Id);
             modelBuilder.Entity<FluentBookDetail>().Property(b => b.NumberOfChapters).IsRequired();
 
-            //Book
-            modelBuilder.Entity<FluentBook>().HasKey(b => b.Book_Id);
-            modelBuilder.Entity<FluentBook>().Property(b => b.ISBN).IsRequired().HasMaxLength(15);
-            modelBuilder.Entity<FluentBook>().Property(b => b.Title).IsRequired();
-            modelBuilder.Entity<FluentBook>().Property(b => b.Price).IsRequired();
-            //one to oone relation between book and bookdetail
-            modelBuilder.Entity<FluentBook>()
-                .HasOne(b => b.FluentBookDetail)
-                .WithOne(b => b.FluentBook).HasForeignKey<FluentBook>("BookDetail_Id");
-            //one to many relation publisher and book
-            modelBuilder.Entity<FluentBook>()
-                .HasOne(b => b.FluentPublisher)
-                .WithMany(b => b.FluentBooks).HasForeignKey(b => b.Publisher_Id);
-            //many to many relation books authors
-            modelBuilder.Entity<FluentBookAuthor>().HasKey(ba => new { ba.AuthorId, ba.BookId });
-            modelBuilder.Entity<FluentBookAuthor>()
-                .HasOne(b => b.FluentBook)
-                .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.BookId);
-            modelBuilder.Entity<FluentBookAuthor>()
-                .HasOne(b => b.FluentAuthor)
-                .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.AuthorId);
+            ////Book
+            //modelBuilder.Entity<FluentBook>().HasKey(b => b.Book_Id);
+            //modelBuilder.Entity<FluentBook>().Property(b => b.ISBN).IsRequired().HasMaxLength(15);
+            //modelBuilder.Entity<FluentBook>().Property(b => b.Title).IsRequired();
+            //modelBuilder.Entity<FluentBook>().Property(b => b.Price).IsRequired();
+            ////one to oone relation between book and bookdetail
+            //modelBuilder.Entity<FluentBook>()
+            //    .HasOne(b => b.FluentBookDetail)
+            //    .WithOne(b => b.FluentBook).HasForeignKey<FluentBook>("BookDetail_Id");
+            ////one to many relation publisher and book
+            //modelBuilder.Entity<FluentBook>()
+            //    .HasOne(b => b.FluentPublisher)
+            //    .WithMany(b => b.FluentBooks).HasForeignKey(b => b.Publisher_Id);
+            ////many to many relation books authors
+            //modelBuilder.Entity<FluentBookAuthor>().HasKey(ba => new { ba.AuthorId, ba.BookId });
+            //modelBuilder.Entity<FluentBookAuthor>()
+            //    .HasOne(b => b.FluentBook)
+            //    .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.BookId);
+            //modelBuilder.Entity<FluentBookAuthor>()
+            //    .HasOne(b => b.FluentAuthor)
+            //    .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.AuthorId);
 
  
 
--- Net5_DataAccess/Data/FluentConfig/FluentBookConfig.cs
+++ Net5_DataAccess/Data/FluentConfig/FluentBookConfig.cs
@@ -9,12 +9,34 @@ using System.Threading.Tasks;
 
 namespace Net5_DataAccess.Data.FluentConfig
 {
-    public class FluentAuthorConfig : IEntityTypeConfiguration<FluentBook>
+    public class FluentBookConfig : IEntityTypeConfiguration<FluentBook>
     {
-        public void Configure(EntityTypeBuilder<FluentBook> modelBulder)
+
+        public void Configure(EntityTypeBuilder<FluentBook> builder)
         {
 
-
+            throw new NotImplementedException();
+            //Book
+            //modelBuilder.Entity<FluentBook>().HasKey(b => b.Book_Id);
+            //modelBuilder.Entity<FluentBook>().Property(b => b.ISBN).IsRequired().HasMaxLength(15);
+            //modelBuilder.Entity<FluentBook>().Property(b => b.Title).IsRequired();
+            //modelBuilder.Entity<FluentBook>().Property(b => b.Price).IsRequired();
+            ////one to oone relation between book and bookdetail
+            //modelBuilder.Entity<FluentBook>()
+            //    .HasOne(b => b.FluentBookDetail)
+            //    .WithOne(b => b.FluentBook).HasForeignKey<FluentBook>("BookDetail_Id");
+            ////one to many relation publisher and book
+            //modelBuilder.Entity<FluentBook>()
+            //    .HasOne(b => b.FluentPublisher)
+            //    .WithMany(b => b.FluentBooks).HasForeignKey(b => b.Publisher_Id);
+            ////many to many relation books authors
+            //modelBuilder.Entity<FluentBookAuthor>().HasKey(ba => new { ba.AuthorId, ba.BookId });
+            //modelBuilder.Entity<FluentBookAuthor>()
+            //    .HasOne(b => b.FluentBook)
+            //    .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.BookId);
+            //modelBuilder.Entity<FluentBookAuthor>()
+            //    .HasOne(b => b.FluentAuthor)
+            //    .WithMany(b => b.FluentBookAuthors).HasForeignKey(b => b.AuthorId);
         }
     }
 }
