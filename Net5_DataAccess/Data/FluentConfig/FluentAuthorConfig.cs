4e1e06dfd40c1f819b5bb06a8d152610ad0a4fbc
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-16 14:36:39 -0300

    fix typo

--- Net5_DataAccess/Data/FluentConfig/FluentAuthorConfig.cs
+++ Net5_DataAccess/Data/FluentConfig/FluentAuthorConfig.cs
@@ -11,7 +11,7 @@ namespace Net5_DataAccess.Data.FluentConfig
 {
     public class FluentAuthorConfig : IEntityTypeConfiguration<FluentAuthor>
     {
-        public void Configure(EntityTypeBuilder<FluentAuthor> modelBulder)
+        public void Configure(EntityTypeBuilder<FluentAuthor> modelBuilder)
         {
             modelBuilder.HasKey(a => a.Author_Id);
             modelBuilder.Property(a => a.FirstName).IsRequired();
