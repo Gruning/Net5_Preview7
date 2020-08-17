e01a971876cd3e20627d8faf4ca6790d75735ced
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-17 10:40:54 -0300

    add registers from migrations

---
+++ Net5_DataAccess/Migrations/20200817133454_add raww category to table.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Net5_DataAccess.Migrations
+{
+    public partial class addrawwcategorytotable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into tbl_Category value('Cat 1')");
+            migrationBuilder.Sql("insert into tbl_Category value('Cat 1')");
+            migrationBuilder.Sql("insert into tbl_Category value('Cat 1')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
