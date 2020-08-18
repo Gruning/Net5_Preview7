0b5fe4961d8349ce080d0a9cb9a8c9d76fcabc5f
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-18 19:13:20 -0300

    sec5cap40_reading

---
+++ Net5_Preview7/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Net5_DataAccess.Data;
+
+namespace Net5_Preview7.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
