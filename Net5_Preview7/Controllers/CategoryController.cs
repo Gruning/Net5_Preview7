63dac2931625c3a191594f07d1cdd4ebf522a1f6
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-22 15:05:11 -0300

    sec5cap40_readingUsingEntity_createView

--- Net5_Preview7/Controllers/CategoryController.cs
+++ Net5_Preview7/Controllers/CategoryController.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Net5_DataAccess.Data;
+using Net5_Model.Models;
 
 namespace Net5_Preview7.Controllers
-{
+{ 
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -16,6 +17,7 @@ namespace Net5_Preview7.Controllers
         }
         public IActionResult Index()
         {
+            List<Category> obj = _db.Categories.ToList();
             return View();
         }
     }
